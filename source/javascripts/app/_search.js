79b5a5271e24760b81c508c64e89ad89c6c6f584
s1ddok <s1ddok@MacBook-Pro-Andrey.local>
2015-12-04 13:44:48 +0300

    translation

--- source/javascripts/app/_search.js
+++ source/javascripts/app/_search.js
@@ -61,7 +61,7 @@
         highlight.call(this);
       } else {
         searchResults.html('<li></li>');
-        $('.search-results li').text('No Results Found for "' + this.value + '"');
+        $('.search-results li').text('Нет совпадений с "' + this.value + '"');
       }
     } else {
       unhighlight();
